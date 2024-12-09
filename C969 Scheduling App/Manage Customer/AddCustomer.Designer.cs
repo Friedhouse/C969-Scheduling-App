2d812b688403d6dc67773959d30b66817e6144fa
Shane Friedman <sfrie53@wgu.edu>
2024-12-09 17:29:44 -0600

    Add customer design complete

--- C969 Scheduling App/Manage Customer/AddCustomer.Designer.cs
+++ C969 Scheduling App/Manage Customer/AddCustomer.Designer.cs
@@ -28,19 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.addCustLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.saveCustBtn = new System.Windows.Forms.Button();
+            this.cancelCustBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(12, 89);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(51, 20);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneLabel.Location = new System.Drawing.Point(12, 189);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(115, 20);
+            this.phoneLabel.TabIndex = 1;
+            this.phoneLabel.Text = "Phone Number";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.Location = new System.Drawing.Point(12, 139);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(68, 20);
+            this.addressLabel.TabIndex = 2;
+            this.addressLabel.Text = "Address";
+            // 
+            // addCustLabel
+            // 
+            this.addCustLabel.AutoSize = true;
+            this.addCustLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCustLabel.Location = new System.Drawing.Point(87, 37);
+            this.addCustLabel.Name = "addCustLabel";
+            this.addCustLabel.Size = new System.Drawing.Size(148, 25);
+            this.addCustLabel.TabIndex = 3;
+            this.addCustLabel.Text = "Add Customer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBox1.Location = new System.Drawing.Point(86, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 189);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 129);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(235, 42);
+            this.textBox3.TabIndex = 6;
+            // 
+            // saveCustBtn
+            // 
+            this.saveCustBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveCustBtn.Location = new System.Drawing.Point(155, 238);
+            this.saveCustBtn.Name = "saveCustBtn";
+            this.saveCustBtn.Size = new System.Drawing.Size(80, 37);
+            this.saveCustBtn.TabIndex = 7;
+            this.saveCustBtn.Text = "Save";
+            this.saveCustBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelCustBtn
+            // 
+            this.cancelCustBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelCustBtn.Location = new System.Drawing.Point(241, 238);
+            this.cancelCustBtn.Name = "cancelCustBtn";
+            this.cancelCustBtn.Size = new System.Drawing.Size(80, 37);
+            this.cancelCustBtn.TabIndex = 8;
+            this.cancelCustBtn.Text = "Cancel";
+            this.cancelCustBtn.UseVisualStyleBackColor = true;
             // 
             // AddCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(338, 300);
+            this.Controls.Add(this.cancelCustBtn);
+            this.Controls.Add(this.saveCustBtn);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addCustLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.nameLabel);
             this.Name = "AddCustomer";
             this.Text = "AddCustomer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label addCustLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button saveCustBtn;
+        private System.Windows.Forms.Button cancelCustBtn;
     }
 }
